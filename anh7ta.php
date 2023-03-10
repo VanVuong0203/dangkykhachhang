c0336188688e5aabe258a4f6570b1d06aa85bd7b
Huỳnh Văn Vương <126145490+VanVuong0203@users.noreply.github.com>
2023-03-10 07:58:52 +0700

    Add files via upload

---
+++ anh7ta.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+   <title> Đăng ký khách hàng </title>
+   <meta charset="UTF-8">
+</head>
+
+<body>
+   <h3>Đăng ký khách hàng</h3>
+   <form action = "" method= "POST">
+   <table border="1" cellpadding="3" cellspacing="0.5">
+        <tr class= "tr1">
+		    <td colspan="3"> Thông tin đăng nhập </td>
+			<tr>
+			    <td>Tên đăng nhập</td>
+				<td><input type="text" placeholder="Tai khoan"></td>
+		    </tr>
+			<tr>
+			    <td> Mật khẩu</td>
+				<td><input type="password" placeholder="Mat khau"></td>
+				<td class ="sao">(*)</td>
+			</tr>
+			<tr>
+			    <td> Nhập lạiMật khẩu</td>
+				<td><input type="password" placeholder="Nhap lai Mat khau"></td>
+				<td class ="mk2">(*)</td>
+			</tr>
+			<tr>
+			    <td> Đăng ký</td>
+				<td><input type="pass" placeholder="Nhap lai Mat khau"></td>
+				<td class ="mk2">(*)</td>
+			</tr>
+			<tr class= "tr1">
+		    <td colspan="3"> Thông tin khách hàng </td>
+	</table>
+	</form>
+</body>
+</html>
---
+++ dangkikhachhang.php
@@ -0,0 +1,40 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="dkkh.css">
+		<title> Đăng kí khách hàng </title>
+	</head>
+	
+	<body>	
+		<h3>Đăng kí khách hàng</h3>
+		<form>
+			<table border="0" cellpadding="0" cellspacing="0">
+				<tr class="tr1">
+					<td colspan="3">Thông tin đăng nhập</td>
+				</tr>
+				
+				<tr>
+					<td>Tên đăng nhập</td>
+					<td><input type="text" placeholder="Tài khoản"></td> 
+					
+				</tr> 
+				
+				<tr>
+					<td>Mật khẩu </td>
+					<td><input type="password" placeholder="Mật khẩu"> </td>
+					<td class="sao">(*)</td>
+					
+				</tr>
+				
+				<tr>
+					<td>Nhập lại mật khẩu</td>
+					<td><input type="password" placeholder="Nhập lại mật khẩu"> </td>
+					<td class="sao">(*)</td>
+				</tr>
+			</table>
+			
+			
+		</form>
+	
+	</body>
+</html>
